789ca78b8380ecf935b1a4619706a376a52abd65
abnercoolman <abnercoolman@gmail.com>
2025-10-24 21:00:46 -0300

    feat(auth): add reusable Input component and register form
    - Implement reusable Input component with styling and integrate it into Register page
    - Add form fields for name, email, password and confirmation

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import Input from "../../form/Input";
+
 function Register() {
+    const [value, setValue] = useState("");
+
+    const handleOnChange = (e) => {
+        setValue(e.target.value);
+    };
+
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Cadastro</h1>
+            <form>
+                <Input
+                    type="text"
+                    text="Nome"
+                    name="name"
+                    placeholder="Digite seu nome"
+                    handleOnChange={handleOnChange}
+                    value={value}
+                />
+                <Input
+                    type="email"
+                    text="Email"
+                    name="email"
+                    placeholder="Digite seu email"
+                    handleOnChange={handleOnChange}
+                    value={value}
+                />
+                <Input
+                    type="password"
+                    text="Senha"
+                    name="password"
+                    placeholder="Digite sua senha"
+                    handleOnChange={handleOnChange}
+                    value={value}
+                />
+                <Input
+                    type="password"
+                    text="Confirmar Senha"
+                    name="confirmPassword"
+                    placeholder="Confirme sua senha"
+                    handleOnChange={handleOnChange}
+                    value={value}
+                />
+                <input type="submit" value="Cadastrar" />
+            </form>
         </div>
     );
 }
