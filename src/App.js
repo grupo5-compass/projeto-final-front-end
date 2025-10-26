4a3ed4b427923972790ecce9e6a34d1bf61e00f4
abnercoolman <abnercoolman@gmail.com>
2025-10-26 20:57:24 -0300

    feat: add flash message component and update auth hook
    - Implementa componente de mensagem flash com funcionalidade de estilização e ocultação automática.
    - Integra mensagens flash com hook de autenticação para exibir feedback de sucesso/erro.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Container from "./components/layout/Container";
+import Message from "./components/layout/Message";
 
 /* Pages */
 import Login from "./components/pages/Auth/Login";
@@ -18,6 +19,7 @@ function App() {
         <Router>
             <UserProvider>
                 <Navbar />
+                <Message />
                 <Container>
                     <Routes>
                         <Route path="/login" element={<Login />} />
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,23 +2,31 @@
 import api from "../utils/api";
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useFlashMessage from "../hooks/useFlashMessage";
 
 export default function useAuth() {
+    const { setFlashMessage } = useFlashMessage();
+
     // Função responsável pela chamada do cadastro
     async function register(user) {
+        let msgTxt = "Usuário criado com sucesso!";
+        let msgType = "success";
+
         try {
-            const data = await api
-                .post("/user", user)
-                .then((response) => {
-                    return response.data;
-                });
+            const data = await api.post("/user", user).then((response) => {
+                return response.data;
+            });
 
             // Teste de resposta da api
             console.log(data);
         } catch (error) {
             // tratar erro
             console.log(error);
+            msgTxt = error.response.data.message;
+            msgType = "error";
         }
+
+        setFlashMessage(msgTxt, msgType);
     }
 
     return { register };
