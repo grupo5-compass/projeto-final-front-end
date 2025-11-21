ab79e95d36dbe3d75e0e08f2590d787b3222ad03
Anelise Bernardy <anelise.dev@gmail.com>
2025-11-21 12:30:24 -0300

    fix(validacao): Sincronizar regras de complexidade de senha com o backend

--- src/utils/passwordValidation.js
+++ src/utils/passwordValidation.js
@@ -10,7 +10,7 @@ export const validatePassword = (password) => {
         uppercase: /[A-Z]/.test(password),
         lowercase: /[a-z]/.test(password),
         number: /[0-9]/.test(password),
-        special: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)
+        special: /[@$!%*?&]/.test(password)
     };
 
     if (!checks.length) {
